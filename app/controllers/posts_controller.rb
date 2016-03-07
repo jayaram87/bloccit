0f895bbffd16c1149c4dd5e54d2a44340371cc8c
jayaram87 <jayaramraja1987@gmail.com>
2016-03-07 05:43:02 +0000

    completed assignment 31

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each do |post|
+      index = 0
+      if index%5 == 0
+        post.title = "SPAM"
+      end
+      index += 1
+    end
   end
 
   def show
