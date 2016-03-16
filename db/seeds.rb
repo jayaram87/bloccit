7ff957f08d1df3d7370839313a9c34ae920ed806
jayaram87 <jayaramraja1987@gmail.com>
2016-03-16 07:27:46 +0000

    revision 30

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,10 @@ posts = Post.all
 end
 
 unique_post = Post.find_or_create_by(title: "Unique Post", body: "Unique Post Body")
-unique_post.comments.create(body: "Unique Comment")
+unique_comment = unique_post.comments.find_or_create_by(body: "Unique Comment")
 
-puts "#{unique_post} unique seed"
+puts "#{unique_post.title} title of Unique post"
+puts "#{unique_comment.body} unique comment"
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
