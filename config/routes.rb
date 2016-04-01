c997e899a7bbf04b5fddd206def2d2487ea17cfe
jayaram87 <jayaramraja1987@gmail.com>
2016-04-01 14:12:15 +0000

    completed checkpint-38

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :questions
 
   resources :users, only: [:new, :create]
+  
+  resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
   
