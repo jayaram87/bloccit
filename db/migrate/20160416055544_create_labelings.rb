cb107e8f681648b69139f39f78bb346ba78a0b6d
jayaram87 <jayaramraja1987@gmail.com>
2016-04-16 14:22:47 +0000

    incomplete checkpoint-42

---
+++ db/migrate/20160416055544_create_labelings.rb
@@ -0,0 +1,9 @@
+class CreateLabelings < ActiveRecord::Migration
+  def change
+    create_table :labelings do |t|
+      t.references :label, index: true
+      t.references :labelable, polymorphic: true, index: true
+    end
+    add_foreign_key :labelings, :labels
+  end
+end
