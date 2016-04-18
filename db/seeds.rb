dbe5d2c38cc194f7cba0ff300e4388c8d597af9a
jayaram87 <jayaramraja1987@gmail.com>
2016-04-18 04:41:33 +0000

    errors in routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,9 @@ topics = Topic.all
 
 
 50.times do
-    Post.create!(user: users.sample, topic: topics.sample, title: RandomData.random_sentence, body: RandomData.random_paragraph)
+    post = Post.create!(user: users.sample, topic: topics.sample, title: RandomData.random_sentence, body: RandomData.random_paragraph)
+    post.update_attribute(:created_at, rand(10.minutes..1.year).ago)
+    rand(1..5).times{post.votes.create!(value: [-1,1].sample, user: users.sample)}
 end
 posts = Post.all
 
@@ -31,5 +33,6 @@ puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{Vote.count} votes created"
 puts "#{Comment.count} comments created"
 puts "#{Question.count} questions created"
