3bafba9c33d8a5bab9da1814d876e3c7a510b609
jayaram87 <jayaramraja1987@gmail.com>
2016-05-10 22:59:43 +0000

    51016

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508030003) do
+ActiveRecord::Schema.define(version: 20160510072740) do
 
   create_table "advertisments", force: :cascade do |t|
     t.string   "title"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 20160508030003) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
