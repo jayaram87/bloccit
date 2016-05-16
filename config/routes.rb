e34b32c9beff0145eb7609432f6af5e95c032d88
jayaram87 <jayaramraja1987@gmail.com>
2016-05-16 00:46:53 +0000

    completed checkpoint-48

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,12 @@ Rails.application.routes.draw do
   get 'welcome/contact'
   
   root 'welcome#index'
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
 
 end
